74506a8c7038b36831fda71e857d908ed66c6087
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-09-14 19:03:45 +0700

    238: configure default servlet handler

--- src/main/java/com/example/springmvc/SpringConfig.java
+++ src/main/java/com/example/springmvc/SpringConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -20,7 +21,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @EnableWebMvc
 @ComponentScan("com.example.springmvc")
 @Configuration
-public class SpringConfig extends WebMvcConfigurationSupport{
+public class SpringConfig extends WebMvcConfigurationSupport {
 
     @Bean
     public ViewResolver viewResolver() {
@@ -33,4 +34,10 @@ public class SpringConfig extends WebMvcConfigurationSupport{
 
         return resolver;
     }
+
+    @Override
+    protected void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
 }
